9178551062a98c6a81355af0a1b665fa3f8d562c
l-palette <l-palette>
2025-05-28 13:06:42 +0500

    Try to create migrations

--- web/env_test.py
+++ web/env_test.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 load_dotenv(dotenv_path)
 db_url = os.getenv("DATABASE_URL")
-print(db_url)
+print(type(db_url))
